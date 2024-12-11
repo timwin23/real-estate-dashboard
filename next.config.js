c9212438119365a96247309710aa962176bfe66b
timwin23 <twinfreyjr@1stclassre.com>
2024-12-10 20:04:51 -0600

    Disable ESLint during build

--- next.config.js
+++ next.config.js
@@ -1,11 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
   eslint: {
     ignoreDuringBuilds: true,
   },
-  experimental: {
-    serverActions: true,
-  }
 }
 
 module.exports = nextConfig
